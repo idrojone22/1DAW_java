bbe6cfca1409383ddf494ba69f32ea80fd95161a
idrojone22 <jordivallspladaw@gmail.com>
2024-11-07 23:15:51 +0100

    Commit automàtic - 07-11-2024 23:15:51

--- tema_4/src/tema4_v4/Cursos.java
+++ tema_4/src/tema4_v4/Cursos.java
@@ -1,21 +1,22 @@
 package tema4_v4;
 
 public abstract class Cursos {
-	 /*ATRIBUTOS*/
+	/* ATRIBUTOS */
 	private String ID_course;
-	 private String titulo;
-	 private String descripcion;
-	 private String categoria;
-	 private String dificultad;
-	 private int precio;
-	 private Fecha fecha_i;
-	 private Fecha fecha_f;
-	 /*ATRIBUTOS*/
-	 
-	 /*MÉTPDPS*/
-	 
-	/*CONSTRUCTOR*/
-	public Cursos(String iD_course, String titulo, String descripcion, String categoria, String dificultad, int precio, Fecha fecha_i2, Fecha fecha_f2) {
+	private String titulo;
+	private String descripcion;
+	private String categoria;
+	private String dificultad;
+	private int precio;
+	private Fecha fecha_i;
+	private Fecha fecha_f;
+	/* ATRIBUTOS */
+
+	/* MÉTPDPS */
+
+	/* CONSTRUCTOR */
+	public Cursos(String iD_course, String titulo, String descripcion, String categoria, String dificultad, int precio,
+			Fecha fecha_i2, Fecha fecha_f2) {
 		this.ID_course = iD_course;
 		this.titulo = titulo;
 		this.descripcion = descripcion;
@@ -25,72 +26,86 @@ public abstract class Cursos {
 		this.fecha_i = fecha_i2;
 		this.fecha_f = fecha_f2;
 	}
-	/*CONSTRUCTOR*/
-	
-	/*GET*/
+	/* CONSTRUCTOR */
+
+	/* GET */
 	String get_ID_course() {
 		return this.ID_course;
 	}
+
 	String get_titulo() {
 		return this.titulo;
 	}
+
 	String get_descripcion() {
 		return this.descripcion;
 	}
+
 	String get_categoria() {
 		return this.categoria;
 	}
+
 	String get_dificultad() {
 		return this.dificultad;
 	}
+
 	int get_precio() {
 		return this.precio;
 	}
+
 	Fecha get_fecha_i() {
 		return this.fecha_i;
 	}
+
 	Fecha get_fecha_f() {
 		return this.fecha_f;
 	}
-	/*GET*/
-	
-	/*SET*/
-	public String  set_ID_course(String ID_course) {
-	    this.ID_course = ID_course;
-	    return ID_course;
+	/* GET */
+
+	/* SET */
+	public String set_ID_course(String ID_course) {
+		this.ID_course = ID_course;
+		return ID_course;
 	}
+
 	public String set_titulo(String titulo) {
-	    this.titulo = titulo;
-	    return titulo;
+		this.titulo = titulo;
+		return titulo;
 	}
+
 	public String set_descripcion(String descripcion) {
-	    this.descripcion = descripcion;
-	    return descripcion;
+		this.descripcion = descripcion;
+		return descripcion;
 	}
+
 	public String set_categoria(String categoria) {
-	    this.categoria = categoria;
-	    return categoria;
+		this.categoria = categoria;
+		return categoria;
 	}
+
 	public String set_dificultad(String dificultad) {
-	    this.dificultad = dificultad;
-	    return dificultad;
+		this.dificultad = dificultad;
+		return dificultad;
 	}
+
 	public int set_precio(int precio) {
-	    this.precio = precio;
-	    return precio;
+		this.precio = precio;
+		return precio;
 	}
+
 	public Fecha set_fecha_i(Fecha fecha_i) {
-	    this.fecha_i = fecha_i;
-	    return fecha_i;
+		this.fecha_i = fecha_i;
+		return fecha_i;
 	}
+
 	public Fecha set_fecha_f(Fecha fecha_f) {
-	    this.fecha_f = fecha_f;
-	    return fecha_f;
+		this.fecha_f = fecha_f;
+		return fecha_f;
 	}
-	/*SET*/
-	
-	/*TOSTRING*/
-	 public abstract String toString();
-	/*TOSTRING*/
-	 
+	/* SET */
+
+	/* TOSTRING */
+	public abstract String toString();
+	/* TOSTRING */
+
 }
--- tema_4/src/tema4_v4/Desplegamiento_web.java
+++ tema_4/src/tema4_v4/Desplegamiento_web.java
@@ -2,19 +2,19 @@ package tema4_v4;
 
 public class Desplegamiento_web extends Cursos {
 
-	/*ATRIBUTOS*/
+	/* ATRIBUTOS */
 	private Fecha fecha_ultimas_plazas_i;
 	private Fecha fecha_ultimas_plazas_f;
-	/*ATRIBUTOS*/
-	
-	/*CONSTRUCTOR*/
+	/* ATRIBUTOS */
+
+	/* CONSTRUCTOR */
 	public Desplegamiento_web(String iD_course, String titulo, String descripcion, String categoria, String dificultad,
 			int precio, Fecha fecha_i, Fecha fecha_f, Fecha fecha_ultimas_plazas_i2, Fecha fecha_ultimas_plazas_f2) {
 		super(iD_course, titulo, descripcion, categoria, dificultad, precio, fecha_i, fecha_f);
 		this.fecha_ultimas_plazas_i = fecha_ultimas_plazas_i2;
 		this.fecha_ultimas_plazas_f = fecha_ultimas_plazas_f2;
 	}
-	/*CONSTRUCTOR*/
+	/* CONSTRUCTOR */
 
 	public Fecha get_fecha_ultimas_plazas_i() {
 		return fecha_ultimas_plazas_i;
@@ -24,28 +24,27 @@ public class Desplegamiento_web extends Cursos {
 		return fecha_ultimas_plazas_f;
 	}
 
-	
 	public Fecha set_fecha_ultimas_plazas_i(Fecha fecha_ultimas_plazas_i) {
 		this.fecha_ultimas_plazas_i = fecha_ultimas_plazas_i;
 		return this.fecha_ultimas_plazas_i;
 	}
-	
+
 	public Fecha set_fecha_ultimas_plazas_f(Fecha fecha_ultimas_plazas_f) {
 		this.fecha_ultimas_plazas_f = fecha_ultimas_plazas_f;
 		return this.fecha_ultimas_plazas_f;
 	}
-	
-	/*TOSTRING*/	
-	public String toString() { 
+
+	/* TOSTRING */
+	public String toString() {
 		return "ID_Course: " + get_ID_course() + "\n" +
-						"titulo: " + get_titulo() + "\n" +
-						"descripcion: " + get_descripcion() + "\n" +
-						"categorio: " + get_dificultad() + "\n" +
-						"precio: " + get_precio() + "\n" +
-						"fecha_i: " + get_fecha_i() + "\n" +
-						"fecha_f: " + get_fecha_f() + "\n" +
-						"fecha_ultimas_plazas_i: " + this.get_fecha_ultimas_plazas_i() + "\n" +
-						"fecha_ultimas_plazas_f: " + this.get_fecha_ultimas_plazas_i();
+				"titulo: " + get_titulo() + "\n" +
+				"descripcion: " + get_descripcion() + "\n" +
+				"categorio: " + get_dificultad() + "\n" +
+				"precio: " + get_precio() + "\n" +
+				"fecha_i: " + get_fecha_i() + "\n" +
+				"fecha_f: " + get_fecha_f() + "\n" +
+				"fecha_ultimas_plazas_i: " + this.get_fecha_ultimas_plazas_i() + "\n" +
+				"fecha_ultimas_plazas_f: " + this.get_fecha_ultimas_plazas_i();
 	}
-	/*TOSTRING*/
+	/* TOSTRING */
 }
