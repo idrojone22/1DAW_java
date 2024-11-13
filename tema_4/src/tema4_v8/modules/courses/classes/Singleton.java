9e9cb25032a78e751da92bd977687069869933a0
Idrojone22 <jordivallspladaw@gmail.com>
2024-11-13 14:38:50 +0100

    Commit automàtic - 13-11-2024 14:38:49

---
+++ tema_4/src/tema4_v8/modules/courses/classes/Singleton.java
@@ -0,0 +1,12 @@
+package tema4_v8.modules.courses.classes;
+
+import java.util.ArrayList;
+
+public class Singleton {
+	
+	public static ArrayList <Desarrollo_web> array_desarrollo;
+	public static ArrayList <Diseno_web> array_diseno;
+	public static ArrayList <Desplegamiento_web> array_desplegamiento;
+	public static String ID_course;
+	
+}
