cf2e07dead6f196a0b64a05fb395b32d6faa7f8b
idrojone22 <jordivallspladaw@gmail.com>
2024-11-26 17:48:00 +0100

    Commit automàtic - 26-11-2024 17:47:59

--- tema_4/src/tema4_v13/modules/users/classes/Singleton.java
+++ tema_4/src/tema4_v13/modules/users/classes/Singleton.java
@@ -1,5 +1,10 @@
 package tema4_v13.modules.users.classes;
 
+import java.util.ArrayList;
+
 public class Singleton {
 
+    public static ArrayList <Admin> array_admin;
+    public static ArrayList <Client> array_client;
+    public static String  username;
 }
