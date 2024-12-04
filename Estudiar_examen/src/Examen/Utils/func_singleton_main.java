9d83a72d3ec9adf6d47e139a17e4ada2f6ad76cb
idrojone22 <jordivallspladaw@gmail.com>
2024-12-04 17:43:04 +0100

    Commit automàtic - 04-12-2024 17:43:04

--- Estudiar_examen/src/Examen/Utils/func_singleton_main.java
+++ Estudiar_examen/src/Examen/Utils/func_singleton_main.java
@@ -1,5 +1,8 @@
 package Examen.Utils;
 
 public class func_singleton_main {
-
+	public static void crear_singleton() {
+		Singleton_cursos.array_desarrollo = new ArrayList <Desarrollo_web> ();
+		Singleton_cursos.array_desarrollo = new ArrayList <Desarrollo_web> ();
+	}
 }
--- Estudiar_examen/src/Examen/main.java
+++ Estudiar_examen/src/Examen/main.java
@@ -3,8 +3,8 @@ package Examen;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		func_singleton_main.crear_singleton();
 	}
 
 }
